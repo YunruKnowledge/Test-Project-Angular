f4542cefae3175d3732e1d799aa4ef48348c5981
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2022-12-04 18:49:11 +0100

    js for btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {}
+export class AppComponent {
+  
+  tohoVote_btn() {
+    window.location.href = ""
+  }
+}
